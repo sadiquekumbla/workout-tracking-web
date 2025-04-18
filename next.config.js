f13427a4bb5cc054966bab8f818e549b530dc5b5
sadiquekumbla <152009134+sadiquekumbla@users.noreply.github.com>
2025-04-18 14:03:00 +0530

    Update Next.js config for static export to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 
